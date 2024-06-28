0d97abd05a6de950aa54792900861aef06e4db12
Udit Jethva <code.uditjethva@gmail.com>
2024-06-28 13:33:36 +0530

    some tweaks

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
 from glob_vars import *
+from tqdm import tqdm
 
 def get_filter_space(space, gw_ind, cc_ind):
 
     return set(gw_col_matrix[gw_ind][cc_ind]).intersection(space)
 
 def max_min_algo(g_space, s_space):
+
+    if(len(s_space) <= 12):
+        return max_min_algo_brute(g_space, s_space)
 
     moves = []
 
@@ -21,14 +25,45 @@ def max_min_algo(g_space, s_space):
         
         
         moves.append((maxlen, (- 1 * int(gw_ind in s_space), ind_to_word[gw_ind])))
+        
     
     moves.sort(reverse=False)
 
     return moves
+   
+def max_min_algo_brute(g_space, s_space):
+
+    moves = []
+
+    for gw_ind in g_space:
+
+        maxlen = -1
+
+        for sw_ind in s_space:
+
+            colcomb = get_color_comb(ind_to_word[gw_ind], ind_to_word[sw_ind])
+
+            ci_len = len(get_filter_space(s_space, gw_ind, ccomb_to_ind[colcomb]))
+
+            if(ci_len != 0):
+                maxlen = max(maxlen, ci_len)
+        
+        
+        moves.append((maxlen, (- 1 * int(gw_ind in s_space), ind_to_word[gw_ind])))
+
+
+    moves.sort(reverse=False)    
+    return moves
 
 def avg_split_algo(g_space, s_space):
 
+    if(len(s_space) <= 12):
+        return max_min_algo_brute(g_space, s_space)
+
     moves = []
+
+    if(len(s_space) <= 12):
+        return avg_split_algo_brute(g_space, s_space)
 
     for gw_ind in g_space:
 
@@ -43,8 +78,32 @@ def avg_split_algo(g_space, s_space):
                 exp_val += ((ci_len ** 2)/len(s_space))
         
         moves.append((exp_val, (-1 * int(gw_ind in s_space), ind_to_word[gw_ind])))
-    
+
     moves.sort(reverse = False)
+
+    return moves
+
+def avg_split_algo_brute(g_space, s_space):
+
+    moves = []
+
+    for gw_ind in g_space:
+
+        exp_val = 0
+
+        for sw_ind in s_space:
+
+            ccomb = get_color_comb(ind_to_word[gw_ind], ind_to_word[sw_ind])
+
+            ci_len = len(get_filter_space(s_space, gw_ind, ccomb_to_ind[ccomb]))
+
+            if(ci_len != 0):
+
+                exp_val += ((ci_len ** 2)/len(s_space))
+            
+        moves.append((exp_val, (-1 * int(gw_ind in s_space), ind_to_word[gw_ind])))
+        
+    moves.sort(reverse=False)
 
     return moves
 
